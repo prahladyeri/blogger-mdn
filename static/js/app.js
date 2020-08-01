503af8b1189f45f70c2e52111af5e9c87472b8ed
Prahlad Yeri <prahladyeri@yahoo.com>
2020-08-01 17:26:52 +0530

    chore: init repo

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+/**
+* Main script file for storing all functions related to blogger-mdn
+* 
+* @author Prahlad Yeri<prahladyeri@yahoo.com>
+* @license Apache-2.0
+* @date
+* created 2020-08-01
+*/
+
+window.addEventListener("DOMContentLoaded", function(){
+	console.log("domcontent loaded!");
+});
