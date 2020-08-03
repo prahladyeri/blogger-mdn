dfd9689ad445be042b3119e5e601d4e1424df70a
Prahlad Yeri <prahladyeri@yahoo.com>
2020-08-03 09:10:57 +0530

    chore: updated dependencies, created routing structure

--- static/js/app.js
+++ static/js/app.js
@@ -7,6 +7,47 @@
 * created 2020-08-01
 */
 
+/**
+* Handle the window.hashchange event
+*/
+function hashchange() {
+	//console.log("onhashchange:", location.hash);
+	var hash = location.hash;
+	var path = hash.substring(1);
+	var title = path[0].toUpperCase() + path.substring(1);
+	$("#lblRoute").text("/"+path);
+	console.log('title', title);
+	document.title = title + " - " + document.title;
+	switch(path) {
+		case "connect":
+			/*@todo: check if connected to blogger
+			if not, prompt for connection.
+			if yes, fill blogs dropdown, set current blog, display list from current blog
+			*/
+		case "about":
+		default:
+	}
+}
+
+/**
+* Initialize the app
+*/
+function init() {
+	//change_route('home')
+	$(window).on('hashchange', hashchange);
+	
+	if (location.hash=="") {
+		location.hash = "#connect";
+	}
+	else {
+		$(window).trigger("hashchange");
+	}
+}
+
+/**
+* Handle the DOMContentLoaded event
+*/
 window.addEventListener("DOMContentLoaded", function(){
 	console.log("domcontent loaded!");
+	init();
 });
